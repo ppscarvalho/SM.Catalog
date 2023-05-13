bf801ab90f001cf197e6a6a378a7243c565ce0e4
Pedro Carvalho <ppscarvalho@gmail.com>
2023-05-13 01:47:32 -0300

    Remove projeto SM.Catalog.Infrastructe

---
+++ src/Infrastructe/SM.Catalog.Infrastructe/Extensions/MediatorExtension.cs
@@ -0,0 +1,30 @@
+﻿using SM.Resource.Communication.Mediator;
+using SM.Resource.Domain;
+
+namespace SM.Catalog.Infrastructe.Extensions
+{
+    public static class MediatorExtension
+    {
+        public static async Task PublishEvent(this IMediatorHandler mediator, CatalogDbContext ctx)
+        {
+            var domainEntities = ctx.ChangeTracker
+                .Entries<Entity>()
+                .Where(x => x.Entity.Notifications != null && x.Entity.Notifications.Any());
+
+            var domainEvents = domainEntities
+                .SelectMany(x => x.Entity.Notifications)
+                .ToList();
+
+            domainEntities.ToList()
+                .ForEach(entity => entity.Entity.ClearEvent());
+
+            var tasks = domainEvents
+                .Select(async (domainEvent) =>
+                {
+                    await mediator.PublishEvent(domainEvent);
+                });
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
---
+++ src/Infrastructe/SM.Catalog.Infrastructe/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,47 @@
+﻿#nullable disable
+
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SM.Catalog.Core.Application.Extensions;
+using SM.Catalog.Core.Application.Interfaces.Repositories.Domain;
+using SM.Resource.Data;
+using SM.Resource.Messagens.CommonMessage.Notifications;
+
+namespace SM.Catalog.Infrastructe.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddApplicationLayer(configuration);
+            services.AddDatabasePersistence(configuration);
+
+            // Repositories
+            services.AddRepositories();
+
+            // Notifications
+            services.AddScoped<INotificationHandler<DomainNotification>, DomainNotificationHandler>();
+
+            // Context
+            services.AddScoped<CatalogDbContext>();
+            services.AddScoped<IUnitOfWork, CatalogDbContext>();
+        }
+
+        private static void AddDatabasePersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<CatalogDbContext>(options =>
+                options.UseMySQL(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    sqlOptions => sqlOptions.EnableRetryOnFailure(maxRetryCount: 10, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null)
+                ),
+                ServiceLifetime.Scoped);
+        }
+
+        private static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+        }
+    }
+}
