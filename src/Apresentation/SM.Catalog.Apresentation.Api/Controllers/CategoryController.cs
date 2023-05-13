db41979db3315be322e46a10fa5ca2c871ced94b
Pedro Carvalho <ppscarvalho@gmail.com>
2023-05-13 01:35:17 -0300

    Implementa projeto SM.Catalog.Apresentation.Api

---
+++ src/Apresentation/SM.Catalog.Apresentation.Api/Controllers/CategoryController.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SM.Catalog.Apresentation.Api.Controllers.BaseController;
+using SM.Catalog.Core.Application.Commands.Category;
+using SM.Catalog.Core.Application.Models;
+using SM.Catalog.Core.Application.Queries.Category;
+using SM.Resource.Communication.Mediator;
+using SM.Resource.Messagens.CommonMessage.Notifications;
+using SM.Resource.Util;
+
+namespace SM.Catalog.Apresentation.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerConfiguration
+    {
+        private readonly ILogger<CategoryController> _logger;
+        private readonly IMediatorHandler _mediatorHandler;
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediatorQuery;
+
+        public CategoryController(
+            ILogger<CategoryController> logger,
+            INotificationHandler<DomainNotification> notifications,
+            IMediatorHandler mediatorHandler,
+            IMapper mapper,
+            IMediator mediatorQuery) : base(notifications, mediatorHandler)
+        {
+            _logger = logger;
+            _mediatorHandler = mediatorHandler;
+            _mapper = mapper;
+            _mediatorQuery = mediatorQuery;
+        }
+
+        [HttpPost]
+        [Route("GetCategoryById")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(CategoryModel), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetCategoryById([FromBody] GetCategoryByIdQuery query)
+        {
+            _logger.LogInformation("Obter todas as categorias");
+            var result = await _mediatorQuery.Send(query);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("GetAllCategory")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(CategoryModel), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllCategory()
+        {
+            _logger.LogInformation("Obter todas as categorias");
+            var result = await _mediatorQuery.Send(new GetAllCategoryQuery());
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("AddCategory")]
+        [ProducesResponseType(typeof(DefaultResult), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(DefaultResult), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(DefaultResult), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<DefaultResult>> SaveCategory([FromBody] CategoryModel categoryModel)
+        {
+            var cmd = _mapper.Map<AddCategoryCommand>(categoryModel);
+            var result = await _mediatorHandler.SendCommand(cmd);
+
+            if (ValidOperation())
+                return Ok(result);
+            else
+                return BadRequest(GetMessageError());
+        }
+
+        [HttpPost]
+        [Route("UpdateCategory")]
+        [ProducesResponseType(typeof(DefaultResult), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(DefaultResult), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(DefaultResult), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<DefaultResult>> UpdateCategory([FromBody] CategoryModel categoryModel)
+        {
+            var cmd = _mapper.Map<UpdateCategoryCommand>(categoryModel);
+            var result = await _mediatorHandler.SendCommand(cmd);
+
+            if (ValidOperation())
+                return Ok(result);
+            else
+                return BadRequest(GetMessageError());
+        }
+    }
+}
---
+++ src/Apresentation/SM.Catalog.Apresentation.Api/Controllers/RPCCategoryController.cs
@@ -0,0 +1,99 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SM.Catalog.Core.Application.Models;
+using SM.Catalog.Core.Application.Queries.Category;
+using SM.MQ.Models;
+using SM.MQ.Models.Categoria;
+using SM.MQ.Operators;
+
+namespace SM.Catalog.Apresentation.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RPCCategoryController : ControllerBase
+    {
+        private readonly ILogger<RPCCategoryController> _logger;
+        private readonly IMapper _mapper;
+        private readonly IPublish _publish;
+
+        public RPCCategoryController(
+            ILogger<RPCCategoryController> logger,
+            IMapper mapper,
+            IPublish publish)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _publish = publish;
+        }
+
+        [HttpPost]
+        [Route("GetCategoryById")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(ResponseCategoriaOut), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetCategoryById([FromBody] GetCategoryByIdQuery query)
+        {
+            var mapIn = new RequestIn
+            {
+                Host = "localhost",
+                Result = query.Id.ToString(),
+                Queue = "GetCategoryById"
+            };
+
+            var result = await _publish.DoRPC<RequestIn, ResponseCategoriaOut>(mapIn);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("GetAllCategory")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(ResponseCategoriaOut), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllCategory()
+        {
+            var mapIn = new RequestIn
+            {
+                Host = "localhost",
+                Queue = "GetAllCategory"
+            };
+
+            var result = await _publish.DoRPC<RequestIn, ResponseCategoriaOut[]>(mapIn);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("AddCategory")]
+        [ProducesResponseType(typeof(ResponseOut), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseOut), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ResponseOut), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ResponseOut>> AddCategory([FromBody] CategoryModel categoryModel)
+        {
+            var mapIn = new RequestIn
+            {
+                Host = "localhost",
+                Result = JsonConvert.SerializeObject(categoryModel),
+                Queue = "AddCategory"
+            };
+
+            var response = await _publish.DoRPC<RequestIn, ResponseOut>(mapIn);
+            return response;
+        }
+
+        [HttpPost]
+        [Route("UpdateCategory")]
+        [ProducesResponseType(typeof(ResponseOut), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseOut), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ResponseOut), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ResponseOut>> UpdateCategory([FromBody] CategoryModel categoryModel)
+        {
+            var mapIn = new RequestIn
+            {
+                Host = "localhost",
+                Result = JsonConvert.SerializeObject(categoryModel),
+                Queue = "UpdateCategory"
+            };
+
+            var response = await _publish.DoRPC<RequestIn, ResponseOut>(mapIn);
+            return response;
+        }
+    }
+}
